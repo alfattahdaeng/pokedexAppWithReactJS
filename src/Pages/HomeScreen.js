58ef03a39775f8ab32870dbc05edbd1c1e2a2631
mertcancet <61883804+mertcancet@users.noreply.github.com>
2020-12-05 01:29:34 +0300

    Added CSS Homescreen

--- src/Pages/HomeScreen.js
+++ src/Pages/HomeScreen.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Button,
-  Card,
-  CardColumns,
-  Container,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import './HomeScreen.css';
 import axios from 'axios';
 
 const HomeScreen = () => {
@@ -15,7 +9,7 @@ const HomeScreen = () => {
 
   useEffect(() => {
     axios
-      .get('https://pokeapi.co/api/v2/pokemon')
+      .get('https://pokeapi.co/api/v2/pokemon?limit=151')
       .then((res) => {
         setPokemons(res.data.results);
       })
@@ -25,27 +19,40 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <div className='mt-2'>
-      <Card body>
+    <div className='mt-3'>
+      <Card body style={{ height: '90vh', overflow: 'auto' }}>
         <Container>
           <Row>
             {pokemons.map((pokemon) => {
               const pokeId = pokemon.url.split('/')[6];
               return (
-                <Col xs={12} md={6} l={4} key={pokeId}>
-                  <Card style={{ width: '18rem', height: '25rem' }}>
-                    <Card.Img
-                      variant='top'
-                      src={`https://pokeres.bastionbot.org/images/pokemon/${pokeId}.png`}
-                    />
-                    <Card.Body>
-                      <h2>{pokemon.name}</h2>
+                <Col xs={12} md={6} lg={4} key={pokeId} className='mt-3'>
+                  <Card
+                    style={{ width: '18rem', height: '30rem' }}
+                    className='mainscreen-card'
+                  >
+                    <div className='bg-dark'>
+                      <h1 className='bg-dark text-white text-center my-3'>
+                        {pokemon.name}
+                      </h1>
+                      <Card.Img
+                        variant='top'
+                        src={`https://pokeres.bastionbot.org/images/pokemon/${pokeId}.png`}
+                      />
+                      <i className='fas fa-heart fa-3x float-right mr-2 mb-1 mainscreen-card__heardIcon' />
+                    </div>
 
-                      <Button> Catch</Button>
-                      <LinkContainer to={`/details/${pokeId}`}>
-                        <Button className='ml-3'>More Info</Button>
-                      </LinkContainer>
-                      <i className='fas fa-heart' />
+                    <Card.Body>
+                      <Row>
+                        <Col md={4}>
+                          <Button> Catch</Button>
+                        </Col>
+                        <Col >
+                          <LinkContainer to={`/details/${pokeId}`}>
+                            <Button className='ml-3'>More Info</Button>
+                          </LinkContainer>
+                        </Col>
+                      </Row>
                     </Card.Body>
                   </Card>
                 </Col>
