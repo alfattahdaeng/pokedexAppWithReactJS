b1b8f7df48cad4d56ba83750c4334754b3b81d53
mertcancet <61883804+mertcancet@users.noreply.github.com>
2020-12-06 00:45:23 +0300

    Header refactor

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './Header.css';
 const Header = () => {
   return (
     <>
-      <Navbar variant='dark' className='navbar ' fixed='top'>
+      <Navbar variant='dark' expand='lg' className='navbar-bg' fixed='top'>
         <LinkContainer to='/'>
           <Navbar.Brand>
+            {' '}
             <img
               alt=''
               src='/images/pokeball.png'
               width='40'
               height='40'
-              className='d-inline-block align-top'
+              className='d-inline-block '
             />{' '}
+            <p className='d-inline '>Pokemon</p>
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Nav className='mr-auto'>
-          <LinkContainer to='/catchedPokemons'>
-            <Nav.Link>Catched Pokemon</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/details/1'>
-            <Nav.Link>Favorite Pokemon</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        <div className='ml-auto pr-2 pt-2 d-inline'>
-          <p className='d-inline text-white'>TR</p>
-          <label class='switch mx-2'>
-            <input type='checkbox' />
-            <span class='slider round'></span>
-          </label>
-          <p className='d-inline text-white'>EN</p>
-        </div>
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <LinkContainer to='/catchedPokemons'>
+              <Nav.Link>Catched Pokemon</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/'>
+              <Nav.Link>Favorite Pokemon</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <div className='ml-auto pr-2 pt-2 d-inline'>
+            <p className='d-inline text-white'>TR</p>
+            <label class='switch mx-2'>
+              <input type='checkbox' />
+              <span class='slider round'></span>
+            </label>
+            <p className='d-inline text-white'>EN</p>
+          </div>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
